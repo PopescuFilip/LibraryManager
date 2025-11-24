c2ff9afa06663b6bb58c5cca19ee1ce15ddc0a64
FilipPopescu <filippopescu18@gmail.com>
2025-11-24 20:08:51 +0200

    Test

--- src/LibraryManager/Program.cs
+++ src/LibraryManager/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DataMapper;
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine($"{AppSettings.LibraryDbConnectionString}");
+Console.WriteLine($"{AppSettings.Restrictions}");
