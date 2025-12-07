3d85ebf038f854dabb8475104cfe41b370992707
FilipPopescu <filippopescu18@gmail.com>
2025-12-07 19:34:30 +0200

    Added dependency injection

--- src/LibraryManager/Program.cs
+++ src/LibraryManager/Program.cs
@@ -1,7 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DataMapper;
+using DataMapper.MigrationHelpers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ServiceLayer;
+using System.Diagnostics.CodeAnalysis;
 
-Console.WriteLine("Hello, World!");
+[ExcludeFromCodeCoverage]
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
 
-//Console.WriteLine($"{AppSettings.LibraryDbConnectionString}");
-//Console.WriteLine($"{AppSettings.Restrictions}");
+        var entryPoint = GetEntryPoint();
+
+        var provider = entryPoint.Services.GetRequiredService<IClientRestrictionsProvider>();
+
+        var first = provider.GetClientRestrictions();
+        var second = provider.GetPrivilegedClientRestrictions();
+        Console.WriteLine(first);
+        Console.WriteLine();
+        Console.WriteLine(second);
+    }
+
+    private static IHost GetEntryPoint() =>
+        Host
+        .CreateDefaultBuilder()
+        .ConfigureServices((context, services) =>
+        {
+            AddDataMapperDependencies(services);
+            AddServiceLayerDependencies(services);
+        })
+        .ConfigureAppConfiguration(builder =>
+        {
+            builder.AddConfiguration(AppSettings.Configuration);
+        })
+        .Build();
+
+    private static IServiceCollection AddDataMapperDependencies(IServiceCollection services) =>
+        services
+        .AddDbContextFactory<LibraryDbContext, LibraryDbContextFactory>()
+        .AddScoped<IRestrictionsProvider, RestrictionsProvider>();
+
+    private static IServiceCollection AddServiceLayerDependencies(IServiceCollection services) =>
+        services
+        .AddScoped<IClientRestrictionsProvider, ClientRestrictionsProvider>();
+}
