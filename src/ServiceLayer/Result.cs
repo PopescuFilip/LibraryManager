dfc412f9ee87e148fe737a86281b29bdbfd861a9
FilipPopescu <filippopescu18@gmail.com>
2025-12-27 16:18:53 +0200

    docs are for nerds

--- src/ServiceLayer/Result.cs
+++ src/ServiceLayer/Result.cs
@@ -2,6 +2,11 @@
 
 public record Result<T>(T? Value, bool IsValid)
 {
+    /// <summary>
+    /// Checks if <see cref="Value"/> is not null and returns it. Throws otherwise.
+    /// </summary>
+    /// <returns><see cref="Value"/> non-nullable</returns>
+    /// <exception cref="NullReferenceException"></exception>
     public T Get() => Value is null ? throw new NullReferenceException() : Value;
 
     public static Result<T> Valid(T Value) => new(Value, true);
