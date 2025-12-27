592ac1ba663d0d8ea7f58c56837d59619435be14
FilipPopescu <filippopescu18@gmail.com>
2025-12-27 14:58:51 +0200

    Tests for validator and cleanup

---
+++ src/ServiceLayer.Tests/DomainValidatorTests.cs
@@ -0,0 +1,64 @@
+﻿using DomainModel;
+using FluentValidation.TestHelper;
+using ServiceLayer.Domains;
+
+namespace ServiceLayer.UnitTests;
+
+[TestClass]
+public class DomainValidatorTests
+{
+    public DomainValidator domainValidator = default!;
+
+    [TestInitialize]
+    public void Init()
+    {
+        domainValidator = new DomainValidator();
+    }
+
+    [TestMethod]
+    public void DomainValidator_ShouldReturnInvalid_WhenNameIsEmpty()
+    {
+        var domain = new Domain("");
+
+        var result = domainValidator.TestValidate(domain);
+
+        result.ShouldHaveValidationErrorFor(d => d.Name);
+    }
+
+    [TestMethod]
+    public void DomainValidator_ShouldReturnInvalid_WhenParentIdIsCurrentId()
+    {
+        var id = 1234;
+        var domain = new Domain("Name", id) { Id = id };
+
+        var result = domainValidator.TestValidate(domain);
+
+        result.ShouldHaveValidationErrorFor(d => d.ParentDomainId);
+    }
+
+    [TestMethod]
+    public void DomainValidator_ShouldReturnInvalid_WhenSubDomainsContainCurrent()
+    {
+        var id = 1234;
+        var name = "Name here";
+        var domain = new Domain(name) { Id = id };
+        domain.SubDomains.Add(domain);
+
+        var result = domainValidator.TestValidate(domain);
+
+        result.ShouldHaveValidationErrorFor(d => d.SubDomains);
+    }
+
+    [TestMethod]
+    public void DomainValidator_ShouldReturnValid_WhenDomainIsInCorrectState()
+    {
+        var id = 1234;
+        var parentId = 23;
+        var name = "Some Name";
+        var domain = new Domain(name, parentId) { Id = id };
+
+        var result = domainValidator.TestValidate(domain);
+
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
