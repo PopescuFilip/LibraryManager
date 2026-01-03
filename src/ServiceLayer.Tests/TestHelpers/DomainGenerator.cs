39f4550ab41da251675a0814996385904286bf4a
PopescuFilip <147136280+PopescuFilip@users.noreply.github.com>
2026-01-03 20:34:11 +0200

    Created DomainQueryServiceTests (#9)

---
+++ src/ServiceLayer.Tests/TestHelpers/DomainGenerator.cs
@@ -0,0 +1,11 @@
+﻿using DomainModel;
+
+namespace ServiceLayer.UnitTests.TestHelpers;
+
+public static class DomainGenerator
+{
+    public static List<Domain> From(List<(int Id, string Name)> idsWithName) =>
+        idsWithName
+        .Select(kvp => new Domain(kvp.Name) { Id = kvp.Id })
+        .ToList();
+}
