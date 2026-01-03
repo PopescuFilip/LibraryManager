da7caa228ac03ea5213c020811d3a7c331a38072
FilipPopescu <filippopescu18@gmail.com>
2026-01-03 22:41:52 +0200

    Complete tests for BookDefinitionCreationValidator

--- src/ServiceLayer.Tests/BookDefinitionCreationValidatorTests.cs
+++ src/ServiceLayer.Tests/BookDefinitionCreationValidatorTests.cs
@@ -42,4 +42,56 @@ public class BookDefinitionCreationValidatorTests
 
         validationResult.ShouldHaveValidationErrorFor(x => x.AuthorIds.Length);
     }
+
+    [TestMethod]
+    public void Validator_ShouldReturnInvalid_WhenDomainIdsIsEmpty()
+    {
+        var name = "name";
+        var authorIds = ImmutableArray.Create(1, 2, 3);
+        var domainIds = ImmutableArray.Create<int>();
+        var options = new BookDefinitionCreateOptions(name, authorIds, domainIds);
+
+        var validationResult = _validator.TestValidate(options);
+
+        validationResult.ShouldHaveValidationErrorFor(x => x.DomainIds.Length);
+    }
+
+    [TestMethod]
+    public void Validator_ShouldReturnInvalid_WhenAuthorIdsContainsDuplicates()
+    {
+        var name = "name";
+        var authorIds = ImmutableArray.Create(1, 2, 3, 3);
+        var domainIds = ImmutableArray.Create(4, 5, 6, 7);
+        var options = new BookDefinitionCreateOptions(name, authorIds, domainIds);
+
+        var validationResult = _validator.TestValidate(options);
+
+        validationResult.ShouldHaveValidationErrorFor(x => x.AuthorIds);
+    }
+
+    [TestMethod]
+    public void Validator_ShouldReturnInvalid_WhenDomainIdsContainsDuplicates()
+    {
+        var name = "name";
+        var authorIds = ImmutableArray.Create(1, 2, 3);
+        var domainIds = ImmutableArray.Create(4, 5, 6, 7, 4);
+        var options = new BookDefinitionCreateOptions(name, authorIds, domainIds);
+
+        var validationResult = _validator.TestValidate(options);
+
+        validationResult.ShouldHaveValidationErrorFor(x => x.DomainIds);
+    }
+
+    [TestMethod]
+    public void Validator_ShouldReturnValid_WhenOptionsAreInCorrectState()
+    {
+        var name = "name";
+        var authorIds = ImmutableArray.Create(1, 2, 3);
+        var domainIds = ImmutableArray.Create(4, 5, 6, 7);
+        var options = new BookDefinitionCreateOptions(name, authorIds, domainIds);
+
+        var validationResult = _validator.TestValidate(options);
+
+        validationResult.ShouldNotHaveAnyValidationErrors();
+    }
 }
