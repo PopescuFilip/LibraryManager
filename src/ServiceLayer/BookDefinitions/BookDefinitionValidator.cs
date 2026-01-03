4b18faaff6dead5e428010a5eeb80b15c9a816f1
FilipPopescu <filippopescu18@gmail.com>
2026-01-04 01:13:13 +0200

    Refining validator

--- src/ServiceLayer/BookDefinitions/BookDefinitionValidator.cs
+++ src/ServiceLayer/BookDefinitions/BookDefinitionValidator.cs
@@ -6,21 +6,24 @@ namespace ServiceLayer.BookDefinitions;
 
 public class BookDefinitionValidator : AbstractValidator<BookDefinition>
 {
-    public BookDefinitionValidator(IDomainQueryService _domainQueryService,
+    public BookDefinitionValidator(
+        IDomainQueryService _domainQueryService,
         IBookRestrictionsProvider _bookRestrictionsProvider)
     {
         RuleFor(x => x.Name).NotEmpty();
         RuleFor(x => x.Authors.Count).GreaterThan(0);
         RuleFor(x => x.Domains.Count).GreaterThan(0);
-        RuleFor(x => x.Domains.Count).LessThanOrEqualTo(_bookRestrictionsProvider.Get().MaxDomains);
-        RuleFor(x => x.Domains).Must(domains =>
-        {
-            var ids = domains.Select(domain => domain.Id);
-            var implicitDomainNames = _domainQueryService.GetImplicitDomainNames(ids)
-            .Distinct()
-            .ToList();
+        RuleFor(x => x.Domains.Count).Must(count => count <= _bookRestrictionsProvider.Get().MaxDomains);
+        RuleFor(x => x.Domains)
+            .Must(domains =>
+            {
+                var ids = domains.Select(domain => domain.Id);
+                var implicitDomainNames = _domainQueryService.GetImplicitDomainNames(ids)
+                .Distinct()
+                .ToList();
 
-            return !domains.Select(d => d.Name).Any(implicitDomainNames.Contains);
-        });
+                return !domains.Select(d => d.Name).Any(implicitDomainNames.Contains);
+            })
+            .When(x => x.Domains.Count > 1);
     }
 }
