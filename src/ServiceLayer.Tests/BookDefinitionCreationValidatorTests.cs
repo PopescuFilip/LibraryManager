493ad98c1c70a014fe5163bcef16d3135048fae4
FilipPopescu <filippopescu18@gmail.com>
2026-01-03 22:33:13 +0200

    Created BookDefinitionCreationValidatorTests

---
+++ src/ServiceLayer.Tests/BookDefinitionCreationValidatorTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation.TestHelper;
+using ServiceLayer.BookDefinitions;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ServiceLayer.UnitTests;
+
+[TestClass]
+[ExcludeFromCodeCoverage]
+public class BookDefinitionCreationValidatorTests
+{
+    private BookDefinitionCreationValidator _validator = default!;
+
+    [TestInitialize]
+    public void Init()
+    {
+        _validator = new BookDefinitionCreationValidator();
+    }
+
+    [TestMethod]
+    public void Validator_ShouldReturnInvalid_WhenNameIsEmpty()
+    {
+        var name = string.Empty;
+        var authorIds = ImmutableArray.Create(1, 2, 3);
+        var domainIds = ImmutableArray.Create(4, 5, 6, 7);
+        var options = new BookDefinitionCreateOptions(name, authorIds, domainIds);
+
+        var validationResult = _validator.TestValidate(options);
+
+        validationResult.ShouldHaveValidationErrorFor(x => x.Name);
+    }
+
+    [TestMethod]
+    public void Validator_ShouldReturnInvalid_WhenAuthorIdsIsEmpty()
+    {
+        var name = "name";
+        var authorIds = ImmutableArray.Create<int>();
+        var domainIds = ImmutableArray.Create(4, 5, 6, 7);
+        var options = new BookDefinitionCreateOptions(name, authorIds, domainIds);
+
+        var validationResult = _validator.TestValidate(options);
+
+        validationResult.ShouldHaveValidationErrorFor(x => x.AuthorIds.Length);
+    }
+}
--- src/ServiceLayer/BookDefinitions/BookDefinitionCreationValidator.cs
+++ src/ServiceLayer/BookDefinitions/BookDefinitionCreationValidator.cs
@@ -8,6 +8,8 @@ public class BookDefinitionCreationValidator : AbstractValidator<BookDefinitionC
     {
         RuleFor(x => x.Name).NotEmpty();
         RuleFor(x => x.AuthorIds.Length).GreaterThan(0);
+        RuleFor(x => x.AuthorIds).Must(ids => ids.Length == ids.Distinct().Count());
         RuleFor(x => x.DomainIds.Length).GreaterThan(0);
+        RuleFor(x => x.DomainIds).Must(ids => ids.Length == ids.Distinct().Count());
     }
 }
