b6aba9249589a1c382aa8521bf41dce4cbca39df
FilipPopescu <filippopescu18@gmail.com>
2026-01-05 23:02:44 +0200

    Created BookEditionServiceTests

---
+++ src/ServiceLayer.Tests/BookEditionServiceTests.cs
@@ -0,0 +1,91 @@
+﻿using DomainModel;
+using FluentValidation;
+using NSubstitute;
+using ServiceLayer.BookEditions;
+using ServiceLayer.CRUD;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ServiceLayer.UnitTests;
+
+[TestClass]
+[ExcludeFromCodeCoverage]
+public class BookEditionServiceTests
+{
+    private BookEditionService _bookEditionService = default!;
+    private IEntityService<BookEdition> _entityService = default!;
+    private IEntityService<BookDefinition> _bookDefinitionService = default!;
+    private IValidator<BookEdition> _validator = default!;
+
+    [TestInitialize]
+    public void Init()
+    {
+        _entityService = Substitute.For<IEntityService<BookEdition>>();
+        _bookDefinitionService = Substitute.For<IEntityService<BookDefinition>>();
+        _validator = Substitute.For<IValidator<BookEdition>>();
+        _bookEditionService = new BookEditionService(
+            _entityService,
+            _bookDefinitionService,
+            _validator);
+    }
+
+    [TestMethod]
+    public void Create_ShouldReturnInvalid_WhenBookDefinitionIsNotFound()
+    {
+        var name = "name";
+        var pagesCount = 123;
+        var bookType = BookType.Paperback;
+        var definitionId = 32;
+        _bookDefinitionService.GetById(definitionId).Returns((BookDefinition?)null);
+        _entityService.Insert(Arg.Any<BookEdition>(), _validator)
+            .Returns(call => Result.Valid(call.Arg<BookEdition>()));
+
+        var result = _bookEditionService.Create(name, pagesCount, bookType, definitionId);
+
+        Assert.IsFalse(result.IsValid);
+    }
+
+    [TestMethod]
+    public void Create_ShouldReturnInvalid_WhenInsertReturnsInvalid()
+    {
+        var name = "name";
+        var pagesCount = 123;
+        var bookType = BookType.Paperback;
+        var definitionId = 32;
+        var existingBookDefinition = new BookDefinition("bookDefinition", [], [])
+        {
+            Id = definitionId
+        };
+        _bookDefinitionService.GetById(definitionId).Returns(existingBookDefinition);
+        _entityService.Insert(Arg.Any<BookEdition>(), _validator)
+            .Returns(Result.Invalid());
+
+        var result = _bookEditionService.Create(name, pagesCount, bookType, definitionId);
+
+        Assert.IsFalse(result.IsValid);
+    }
+
+    [TestMethod]
+    public void Create_ShouldInsertAndReturnBookEdition_WhenAllValidationsPass()
+    {
+        var name = "name";
+        var pagesCount = 123;
+        var bookType = BookType.Paperback;
+        var definitionId = 32;
+        var existingBookDefinition = new BookDefinition("bookDefinition", [], [])
+        {
+            Id =  definitionId
+        };
+        _bookDefinitionService.GetById(definitionId).Returns(existingBookDefinition);
+        _entityService.Insert(Arg.Any<BookEdition>(), _validator)
+            .Returns(call => Result.Valid(call.Arg<BookEdition>()));
+
+        var result = _bookEditionService.Create(name, pagesCount, bookType, definitionId);
+
+        Assert.IsTrue(result.IsValid);
+        var bookEdition = result.Get();
+        Assert.AreEqual(name, bookEdition.Name);
+        Assert.AreEqual(pagesCount, bookEdition.PagesCount);
+        Assert.AreEqual(bookType, bookEdition.BookType);
+        Assert.AreEqual(definitionId, bookEdition.BookDefinitionId);
+    }
+}
