a817619fb97640ae5195bd3f2d8b63823e98f53e
FilipPopescu <filippopescu18@gmail.com>
2026-01-05 22:22:02 +0200

    Created BookEditionService and BookEditionValidator

---
+++ src/ServiceLayer/BookEditions/BookEditionService.cs
@@ -0,0 +1,26 @@
+﻿using DomainModel;
+using ServiceLayer.CRUD;
+
+namespace ServiceLayer.BookEditions;
+
+public interface IBookEditionService
+{
+    Result<BookEdition> Create(string name, int pagesCount, BookType bookType, int bookDefinitionId);
+}
+
+public class BookEditionService(
+    IEntityService<BookEdition> _entityService,
+    IEntityService<BookDefinition> _bookDefinitionService)
+    : IBookEditionService
+{
+    public Result<BookEdition> Create(string name, int pagesCount, BookType bookType, int bookDefinitionId)
+    {
+        var bookDefinition = _bookDefinitionService.GetById(bookDefinitionId);
+
+        if (bookDefinition is null)
+            return Result.Invalid();
+
+        var bookEdition = new BookEdition(name, pagesCount, bookType, bookDefinitionId);
+        return _entityService.Insert(bookEdition, EmptyValidator.Create<BookEdition>());
+    }
+}
---
+++ src/ServiceLayer/BookEditions/BookEditionValidator.cs
@@ -0,0 +1,13 @@
+﻿using DomainModel;
+using FluentValidation;
+
+namespace ServiceLayer.BookEditions;
+
+public class BookEditionValidator : AbstractValidator<BookEdition>
+{
+    public BookEditionValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.PagesCount).GreaterThan(0);
+    }
+}
