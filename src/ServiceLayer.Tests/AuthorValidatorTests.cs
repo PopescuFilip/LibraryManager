47423be94bfaeee9af7b628f9804f90d829749c2
FilipPopescu <filippopescu18@gmail.com>
2026-01-05 19:59:24 +0200

    Created AuthorValidatorTests

---
+++ src/ServiceLayer.Tests/AuthorValidatorTests.cs
@@ -0,0 +1,39 @@
+﻿using DomainModel;
+using FluentValidation.TestHelper;
+using ServiceLayer.Authors;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ServiceLayer.UnitTests;
+
+[TestClass]
+[ExcludeFromCodeCoverage]
+public class AuthorValidatorTests
+{
+    private AuthorValidator _authorValidator = default!;
+
+    [TestInitialize]
+    public void Init()
+    {
+        _authorValidator = new AuthorValidator();
+    }
+
+    [TestMethod]
+    public void Validator_ShouldReturnInvalid_WhenNameIsEmpty()
+    {
+        var author = new Author(string.Empty);
+
+        var result = _authorValidator.TestValidate(author);
+
+        result.ShouldHaveValidationErrorFor(x => x.Name);
+    }
+
+    [TestMethod]
+    public void Validator_ShouldReturnValid_WhenAuthorIsInValidState()
+    {
+        var author = new Author("name");
+
+        var result = _authorValidator.TestValidate(author);
+
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
