e1374d69d8a1820ce66a199754ad90651dd707cb
FilipPopescu <filippopescu18@gmail.com>
2026-01-06 21:58:54 +0200

    Completed BookEditionServiceTests

--- src/ServiceLayer.Tests/BookEditionServiceTests.cs
+++ src/ServiceLayer.Tests/BookEditionServiceTests.cs
@@ -3,6 +3,7 @@ using FluentValidation;
 using NSubstitute;
 using ServiceLayer.BookEditions;
 using ServiceLayer.CRUD;
+using ServiceLayer.UnitTests.TestHelpers;
 using System.Diagnostics.CodeAnalysis;
 
 namespace ServiceLayer.UnitTests;
@@ -93,5 +94,73 @@ public class BookEditionServiceTests
         Assert.AreEqual(pagesCount, bookEdition.PagesCount);
         Assert.AreEqual(bookType, bookEdition.BookType);
         Assert.AreEqual(definitionId, bookEdition.BookDefinitionId);
+    }
+
+    [TestMethod]
+    public void AddBooks_ShouldReturnInvalid_WhenBookEditionIsNotFound()
+    {
+        var options = new BookAddOptions(20, 14, 1);
+        _optionsValidator.Validate(options).Returns(Validation.ValidResult);
+        _queryService.GetByIdWithBooks(options.BookEditionId).Returns((BookEdition?)null);
+        _entityService.Update(Arg.Any<BookEdition>(), _validator)
+            .Returns(call => Result.Valid(call.Arg<BookEdition>()));
+
+        var result = _bookEditionService.AddBooks(options);
+
+        Assert.IsFalse(result.IsValid);
+    }
+
+    [TestMethod]
+    public void AddBooks_ShouldReturnInvalid_WhenOptionsValidationFails()
+    {
+        var options = new BookAddOptions(20, 14, 1);
+        var bookEdition = new BookEdition("nameHere", 321, BookType.LargePrint, options.BookEditionId);
+        _optionsValidator.Validate(options).Returns(Validation.InvalidResult);
+        _queryService.GetByIdWithBooks(options.BookEditionId).Returns(bookEdition);
+        _entityService.Update(bookEdition, _validator)
+            .Returns(call => Result.Valid(call.Arg<BookEdition>()));
+
+        var result = _bookEditionService.AddBooks(options);
+
+        Assert.IsFalse(result.IsValid);
+    }
+
+    [TestMethod]
+    public void AddBooks_ShouldUpdateBookEdition_WhenBooksAreAddedSuccessfully()
+    {
+        var options = new BookAddOptions(20, 14, 1);
+        var bookEdition = new BookEdition("nameHere", 321, BookType.LargePrint, options.BookEditionId);
+        _optionsValidator.Validate(options).Returns(Validation.ValidResult);
+        _queryService.GetByIdWithBooks(options.BookEditionId).Returns(bookEdition);
+        _entityService.Update(bookEdition, _validator)
+            .Returns(call => Result.Valid(call.Arg<BookEdition>()));
+
+        var result = _bookEditionService.AddBooks(options);
+
+        Assert.IsTrue(result.IsValid);
+        var updatedBookEdition = result.Get();
+        var totalCount = options.ForReadingRoomCount + options.ForBorrowingCount;
+        Assert.AreEqual(totalCount, updatedBookEdition.BookRecords.Count());
+        Assert.AreEqual(
+            options.ForReadingRoomCount,
+            updatedBookEdition.BookRecords.Count(x => x.Status == BookStatus.ForReadingRoom));
+        Assert.AreEqual(
+            options.ForBorrowingCount,
+            updatedBookEdition.BookRecords.Count(x => x.Status == BookStatus.Available));
+    }
+
+    [TestMethod]
+    public void AddBooks_ShouldReturnBookEdition_WhenBooksAreAddedSuccessfully()
+    {
+        var options = new BookAddOptions(20, 14, 1);
+        var bookEdition = new BookEdition("nameHere", 321, BookType.LargePrint, options.BookEditionId);
+        _optionsValidator.Validate(options).Returns(Validation.ValidResult);
+        _queryService.GetByIdWithBooks(options.BookEditionId).Returns(bookEdition);
+        _entityService.Update(bookEdition, _validator)
+            .Returns(call => Result.Valid(call.Arg<BookEdition>()));
+
+        var result = _bookEditionService.AddBooks(options);
+
+        Assert.IsTrue(result.IsValid);
     }
 }
