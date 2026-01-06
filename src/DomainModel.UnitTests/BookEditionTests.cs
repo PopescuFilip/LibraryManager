7657b6cdec8d3347bac445251b3ac6ab7936a4a2
FilipPopescu <filippopescu18@gmail.com>
2026-01-06 21:20:02 +0200

    Created unit tests project for DomainModel

---
+++ src/DomainModel.UnitTests/BookEditionTests.cs
@@ -0,0 +1,21 @@
+﻿namespace DomainModel;
+
+[TestClass]
+public class BookEditionTests
+{
+    [TestMethod]
+    public void Constructor_ShouldWork()
+    {
+        var name = "name";
+        var pagesCount = 12;
+        var bookType = BookType.SpiralBound;
+        var bookDefinitionId = 2;
+
+        var bookEdition = new BookEdition(name, pagesCount, bookType, bookDefinitionId);
+
+        Assert.AreEqual(name, bookEdition.Name);
+        Assert.AreEqual(pagesCount, bookEdition.PagesCount);
+        Assert.AreEqual(bookType, bookEdition.BookType);
+        Assert.AreEqual(bookDefinitionId, bookEdition.BookDefinitionId);
+    }
+}
