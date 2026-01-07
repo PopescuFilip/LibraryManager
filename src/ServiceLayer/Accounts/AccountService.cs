23893f33128a6de642bf2e2d86f172f76a606a34
FilipPopescu <filippopescu18@gmail.com>
2026-01-07 16:11:09 +0200

    Created AuthorOptions

--- src/ServiceLayer/Accounts/AccountService.cs
+++ src/ServiceLayer/Accounts/AccountService.cs
@@ -1,7 +1,14 @@
 ﻿using DomainModel;
+using ServiceLayer.Authors;
 using ServiceLayer.CRUD;
 
 namespace ServiceLayer.Accounts;
+
+public interface IAccountService
+{
+    Result<Account> Create(AuthorOptions options);
+    Result<Account> Update(AuthorOptions options);
+}
 
 public class AccountService(IEntityService<Account> _entityService)
 {
---
+++ src/ServiceLayer/Authors/AuthorOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace ServiceLayer.Authors;
+
+public record AuthorOptions(
+    string Name,
+    string Address,
+    string? Email,
+    string? PhoneNumber);
