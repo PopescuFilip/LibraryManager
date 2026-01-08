77b2b354008351881a4bbd51af4d11b0516cf577
FilipPopescu <filippopescu18@gmail.com>
2026-01-08 19:01:48 +0200

    Cleanup

---
+++ src/ServiceLayer/Borrowing/BorrowService.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceLayer.Borrowing;
+
+public interface IBorrowService
+{
+    bool Borrow(int borrowerId, int lenderId, IdCollection bookIds);
+}
+
+public class BorrowService
+{
+}
