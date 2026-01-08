64160f45999405f3f64ffac8a90c0b6e980e1677
FilipPopescu <filippopescu18@gmail.com>
2026-01-08 14:18:57 +0200

    Created ClientRestrictionsProviderTests

---
+++ src/ServiceLayer.Tests/ClientRestrictionsProviderTests.cs
@@ -0,0 +1,36 @@
+﻿using DataMapper;
+using DomainModel.Restrictions;
+using NSubstitute;
+using ServiceLayer.CRUD;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ServiceLayer.UnitTests;
+
+[TestClass]
+[ExcludeFromCodeCoverage]
+public class ClientRestrictionsProviderTests
+{
+    private ClientRestrictionsProvider _clientRestrictionsProvider = default!;
+    private IRestrictionsProvider _restrictionsProvider = default!;
+
+    [TestInitialize]
+    public void Init()
+    {
+        _restrictionsProvider = Substitute.For<IRestrictionsProvider>();
+        _clientRestrictionsProvider = new ClientRestrictionsProvider(_restrictionsProvider);
+    }
+
+    [TestMethod]
+    public void GetClientRestrictions_ShouldReturnCorrectRestrictions()
+    {
+        var restrictions = new Restrictions()
+        {
+
+        };
+        _restrictionsProvider.GetRestrictions().Returns(restrictions);
+
+        var clientRestrictions = _clientRestrictionsProvider.GetClientRestrictions();
+
+        Assert.IsTrue(true);
+    }
+}
