d5f95857d606c8b594724c5d3cfd90b82092de9b
FilipPopescu <filippopescu18@gmail.com>
2026-01-08 13:57:16 +0200

    Created RestrictionsService

---
+++ src/ServiceLayer/Restriction/RestrictionsService.cs
@@ -0,0 +1,26 @@
+﻿using DomainModel.Restrictions;
+using ServiceLayer.CRUD;
+
+namespace ServiceLayer.Restriction;
+
+public interface IRestrictionsService
+{
+    Result<ClientRestrictions> GetRestrictionsForAccount(int accountId);
+}
+
+public class RestrictionsService(
+    IClientRestrictionsProvider _clientRestrictionsProvider,
+    IAccountQueryService _accountQueryService)
+    : IRestrictionsService
+{
+    public Result<ClientRestrictions> GetRestrictionsForAccount(int accountId)
+    {
+        if (!_accountQueryService.ClientForAccountExists(accountId))
+            return Result.Invalid();
+
+        if (_accountQueryService.EmployeeForAccountExists(accountId))
+            return Result.Valid(_clientRestrictionsProvider.GetPrivilegedClientRestrictions());
+
+        return Result.Valid(_clientRestrictionsProvider.GetClientRestrictions());
+    }
+}
