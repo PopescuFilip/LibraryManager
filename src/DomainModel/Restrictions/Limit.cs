f54ecfce53c064c518eafb37279f4d2a7f6cfdf4
FilipPopescu <filippopescu18@gmail.com>
2026-01-08 17:53:32 +0200

    Organizing limits

--- src/DomainModel/Restrictions/Limit.cs
+++ src/DomainModel/Restrictions/Limit.cs
@@ -2,7 +2,17 @@
 
 public enum TimeUnit { Day, Month };
 
-public abstract record Limit(int ItemCount)
+public abstract record BaseLimit(int ItemCount);
+
+public abstract record TimewiseLimit(int ItemCount) : BaseLimit(ItemCount);
+
+public record PerRequestLimit(int ItemCount) : BaseLimit(ItemCount);
+
+public sealed record PerDayLimit(int ItemCount) : TimewiseLimit(ItemCount);
+
+public sealed record PeriodLimit(int ItemCount, int TimeUnitCount, TimeUnit TimeUnit) : TimewiseLimit(ItemCount);
+
+public static class Limit
 {
     public static PerDayLimit PerDay(int ItemCount) => new(ItemCount);
 
@@ -13,34 +23,4 @@ public abstract record Limit(int ItemCount)
 
     public static PeriodLimit PerPeriodInMonths(int ItemCount, int DayCount) =>
         new(ItemCount, DayCount, TimeUnit.Month);
-}
-
-public record PerRequestLimit(int ItemCount) : Limit(ItemCount);
-
-public abstract record TimewiseLimit(int ItemCount) : Limit(ItemCount)
-{
-    public DateTime GetStartTimeToCheck() => this switch
-    {
-        PeriodLimit { TimeUnit: TimeUnit.Day } periodLimit => DateTime.Now.AddDays(-periodLimit.TimeUnitCount),
-        PeriodLimit { TimeUnit: TimeUnit.Month } periodLimit => DateTime.Now.AddMonths(-periodLimit.TimeUnitCount),
-        PerDayLimit => DateTime.Today,
-        _ => throw new InvalidOperationException($"{this} is not supported")
-    };
-}
-
-public sealed record PerDayLimit(int ItemCount) : TimewiseLimit(ItemCount);
-
-public sealed record PeriodLimit(int ItemCount, int TimeUnitCount, TimeUnit TimeUnit) : TimewiseLimit(ItemCount)
-{
-    public PeriodLimit HalfTime() => this switch
-    {
-        (1, _, _) => this,
-        _ => this with { TimeUnitCount = TimeUnitCount / 2 },
-    };
-}
-
-public static class LimitExtensions
-{
-    public static T DoubleItem<T>(this T limit) where T : Limit =>
-        limit with { ItemCount = limit.ItemCount * 2 };
 }
---
+++ src/DomainModel/Restrictions/LimitExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace DomainModel.Restrictions;
+
+public static class LimitExtensions
+{
+    public static T DoubleItem<T>(this T limit) where T : BaseLimit =>
+        limit with { ItemCount = limit.ItemCount * 2 };
+
+    public static PeriodLimit HalfTime(this PeriodLimit limit) => limit switch
+    {
+        (1, _, _) => limit,
+        _ => limit with { TimeUnitCount = limit.TimeUnitCount / 2 },
+    };
+
+    public static DateTime GetStartTimeToCheck(this TimewiseLimit limit) => limit switch
+    {
+        PeriodLimit { TimeUnit: TimeUnit.Day } periodLimit => DateTime.Now.AddDays(-periodLimit.TimeUnitCount),
+        PeriodLimit { TimeUnit: TimeUnit.Month } periodLimit => DateTime.Now.AddMonths(-periodLimit.TimeUnitCount),
+        PerDayLimit => DateTime.Today,
+        _ => throw new InvalidOperationException($"{limit} is not supported")
+    };
+}
