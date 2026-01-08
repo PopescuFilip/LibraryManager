227701291e1fa80d696778ea2e474c01f855e6a4
FilipPopescu <filippopescu18@gmail.com>
2026-01-08 20:06:58 +0200

    Create BorrowRecordQueryService

--- src/DataMapper/QueryHelpers/Collector.cs
+++ src/DataMapper/QueryHelpers/Collector.cs
@@ -7,4 +7,7 @@ public static class Collector<T>
     public static readonly Func<IQueryable<T>, T?> SingleOrDefault = q => q.SingleOrDefault();
 
     public static readonly Func<IQueryable<T>, bool> Any = q => q.Any();
+
+    public static Func<IQueryable<T>, R> From<R>(Func<IQueryable<T>, R> func) =>
+        q => func(q);
 }
---
+++ src/ServiceLayer/CRUD/BorrowRecordQueryService.cs
@@ -0,0 +1,35 @@
+﻿using DataMapper;
+using DataMapper.QueryHelpers;
+using DomainModel;
+using System.Linq.Expressions;
+
+namespace ServiceLayer.CRUD;
+
+public interface IBorrowRecordQueryService
+{
+    int GetBooksLendedTodayCount(int employeeId);
+}
+
+public class BorrowRecordQueryService(IRepository<BorrowRecord> _repository)
+    : IBorrowRecordQueryService
+{
+    public int GetBooksLendedTodayCount(int employeeId)
+    {
+        var today = DateTime.Today;
+        var tomorrow = today.AddDays(1);
+
+        Expression<Func<BorrowRecord, bool>> filter = x =>
+            x.LenderId == employeeId
+            && today <= x.BorrowDateTime
+            && x.BorrowDateTime < tomorrow;
+
+        return _repository.Get(
+            x => x.BorrowedBooks.Count,
+            Collector<int>.From(q => q.Sum()),
+            asNoTracking: true,
+            Order<BorrowRecord>.ById,
+            filter,
+            includeProperties: x => x.BorrowedBooks
+            );
+    }
+}
