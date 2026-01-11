ce66b362abd85fa288dcd7b1815a3ec7eec8571a
FilipPopescu <filippopescu18@gmail.com>
2026-01-11 20:24:35 +0200

    Doing Logging

---
+++ src/ServiceLayer/Logging/NLogLoggerFactory.cs
@@ -0,0 +1,13 @@
+﻿using NLog;
+
+namespace ServiceLayer.Logging;
+
+public interface INLogLoggerFactory
+{
+    ILogger GetLogger<T>();
+}
+
+public class NLogLoggerFactory : INLogLoggerFactory
+{
+    public ILogger GetLogger<T>() => LogManager.GetLogger(typeof(T).ToString());
+}
