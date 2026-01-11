55e03fc36982e572c6f681edeae4bf7b3dd6db91
FilipPopescu <filippopescu18@gmail.com>
2026-01-12 01:33:05 +0200

    Removed unused functions

--- src/ServiceLayer/Optional.cs
+++ src/ServiceLayer/Optional.cs
@@ -12,11 +12,6 @@ public static class Optional
 
     public static Optional<T> None<T>() => new None<T>();
 
-    public static Optional<T> FromNullable<T>(T? value) =>
-        value is null
-        ? new None<T>()
-        : new Some<T>(value);
-
     public static void Apply<T>(this Optional<T> optional, Action<T> action)
     {
         switch (optional)
@@ -40,14 +35,4 @@ public static class Optional
         optional is Some<T> some
         ? some.Value
         : throw new InvalidOperationException();
-
-    public static Optional<R> MapToOptional<R, T>(this Optional<T> optional, Func<T, R> transform) =>
-        optional.Map(x => Some(transform(x)), new None<R>());
-
-    public static R Map<R, T>(this Optional<T> optional, Func<T, R> transform, R defaultValue) => optional switch
-    {
-        Some<T> some => transform(some.Value),
-        None<T> => defaultValue,
-        _ => throw new InvalidOperationException()
-    };
 }
